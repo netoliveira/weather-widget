32448e64870e75cfd40b66dc74309e1cf289dca3
Neto Oliveira <neto@netoliveira.com>
2025-06-05 12:08:36 -0300

    force-cache

--- src/app/repositorios/page.tsx
+++ src/app/repositorios/page.tsx
@@ -7,20 +7,20 @@ async function getReposGitHub() {
   const response = await fetch(
     'https://api.github.com/users/netoliveira/repos',
     {
-      cache: 'no-store',
+      cache: 'force-cache',
     }
   )
   return response.json()
 }
 
 export default async function Repositorios() {
-  const data = await getReposGitHub()
+  const data: getReposGitHubProps[] = await getReposGitHub()
   console.log(data)
   return (
     <div className="flex flex-col text-black">
       <h1 className="text-5xl">Meus repositórios</h1>
       <ul className="mt-10 flex flex-col gap-5">
-        {data.map((item: getReposGitHubProps) => (
+        {data.map((item) => (
           <li key={item.id}>{item.name}</li>
         ))}
       </ul>
