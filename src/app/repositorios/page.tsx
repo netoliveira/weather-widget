0746341487ea0e8d79149a615904a7316ff14208
Neto Oliveira <neto@netoliveira.com>
2025-06-05 12:16:36 -0300

    Suspense

--- src/app/repositorios/page.tsx
+++ src/app/repositorios/page.tsx
@@ -1,15 +1,12 @@
+import { Suspense } from 'react'
+
 interface getReposGitHubProps {
   id: number
   name: string
 }
 
 async function getReposGitHub() {
-  const response = await fetch(
-    'https://api.github.com/users/netoliveira/repos',
-    {
-      cache: 'force-cache',
-    }
-  )
+  const response = await fetch('https://api.github.com/users/netoliveira/repos')
   return response.json()
 }
 
@@ -17,13 +14,15 @@ export default async function Repositorios() {
   const data: getReposGitHubProps[] = await getReposGitHub()
   console.log(data)
   return (
-    <div className="flex flex-col text-black">
-      <h1 className="text-5xl">Meus repositórios</h1>
-      <ul className="mt-10 flex flex-col gap-5">
-        {data.map((item) => (
-          <li key={item.id}>{item.name}</li>
-        ))}
-      </ul>
-    </div>
+    <Suspense fallback="Carregando...">
+      <div className="flex flex-col text-black">
+        <h1 className="text-5xl">Meus repositórios</h1>
+        <ul className="mt-10 flex flex-col gap-5">
+          {data.map((item) => (
+            <li key={item.id}>{item.name}</li>
+          ))}
+        </ul>
+      </div>
+    </Suspense>
   )
 }
